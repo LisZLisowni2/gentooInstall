03d6012b69a2de474740f6ef9f1a4b90f9dc53fa
LisZLisowni2 <rafalkozikowski735@gmail.com>
2024-12-18 18:50:07 +0100

    Hotfix startMenu.cpp

--- src/startMenu.cpp
+++ src/startMenu.cpp
@@ -14,15 +14,4 @@ void InstallerFirst::startMenu() {
     std::cout << "This is the prototype of installer like archinstall.\nIt isn't fully automated, during installation you have to configure yourself but I guarantee you\nI will explain you that you can be yourself.\nGentoo is Linux distribution focused to customization, you can precised to use systemd or openrc,\nconfigure each programe for your needs and so on.\nIf you intercept an error during script, create pull request and describe your problem.\nIt will help us to improve this script.\n\n";
 	std::cout << "If you are ready, click any key to continue\n";
     getch();
-    // getch();
-    // tput setaf 12 
-	// printf " %s" "GentooInstall "
-	// tput sgr0
-	// printf " %-${max_width}s\n\n\n" "script!"
-
-	// print_wrap "This is the prototype of installer like archinstall. It isn't fully automated, during installation you have to configure yourself but I guarantee you I will explain you that you can be yourself. Gentoo is Linux distribution focused to customization, you can precised to use systemd or openrc, configure each programe for your needs and so on. If you intercept an error during script, create pull request and describe your problem. It will help us to improve this script."
-	
-	// printf "\n\n"
-	// printf "%-${max_width}s\n" "If you are ready, click any key to continue"
-	// read -rsn1 any
 }
