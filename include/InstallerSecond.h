bb2688e0f73fd3d8da36d4beacacb4e17b3d92ca
lislisowni2 <rafalkozikowski735@gmail.com>
2025-08-24 14:17:31 +0200

    Update user creation, big update

--- include/InstallerSecond.h
+++ include/InstallerSecond.h
@@ -19,8 +19,20 @@ private:
     inline void kernelCompile();
     inline void networkConfig();
     inline void bootloader();
+    inline void isValidUsername(const std::string& username);
+    inline void userExists(const std::string& username);
     inline void userCreation();
     inline int profileSelection();
+};
+
+class UsernameInvalid : public std::exception {
+    private:
+        char* message;
+    public:
+        UsernameInvalid(char* msg) : message(msg) {}
+        char* what() {
+            return message;
+        }
 };
 
 #endif
