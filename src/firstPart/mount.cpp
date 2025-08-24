fa2aee674bde6aabba3ae6e5062b9082dd081e5d
lislisowni2 <rafalkozikowski735@gmail.com>
2025-08-24 13:15:39 +0200

    Update mount.cpp

--- src/firstPart/mount.cpp
+++ src/firstPart/mount.cpp
@@ -19,28 +19,22 @@ void InstallerFirst::mount() {
         int key = selectMenu(options, "Mouting partitions", "Mounted partitions means that you can directly access to data on. In this step mount every ppartition that you created. IMPORTANT! START FROM ROOT PARTITION! If you created home partition or any else doesn't mention before use Mount another option and define your own directory. If you have already mounted the partitions (like previous step) skip this. If you want manually mount partitions click Ctrl+C and come back after.");
         std::cout << "\n";
         std::string partition;
-        executeCommand("fdisk -l");
+        partition = partitionSelection();
         switch (key) {
             case -1:
                 return;
                 break;
             case 0:
-                std::cout << "\n\nInput root partition (e.g. /dev/sda1): ";
-                std::cin >> partition;
                 executeCommand("mount " + partition + " /mnt/gentoo");
                 break;
             case 1:
-                std::cout << "\n\nInput boot partition (e.g. /dev/sda2): ";
-                std::cin >> partition;
                 if (isEFI) executeCommand("mount --mkdir " + partition + " /mnt/gentoo/boot/efi");
                 else executeCommand("mount --mkdir " + partition + " /mnt/gentoo/boot/");
                 break;
             case 2:
                 std::string path;
-                std::cout << "\n\nInput partition that you want mount: ";
-                std::cin >> partition;
                 std::cout << "\nInput path where mount: ";
-                std::cin >> path;
+                std::getline(std::cin, path);
                 executeCommand("mount --mkdir " + partition + " " + path);
                 break;
             }
