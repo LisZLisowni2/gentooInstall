6dd8845d148af0f36daa5b0e2c7063cd81c9bb4e
lislisowni2 <rafalkozikowski735@gmail.com>
2025-08-24 16:21:36 +0200

    Hotfix chroot.cpp

--- src/firstPart/chroot.cpp
+++ src/firstPart/chroot.cpp
@@ -12,5 +12,5 @@ void InstallerFirst::chroot() {
     executeCommand("cp --dereference /etc/resolv.conf /mnt/gentoo/etc");
     executeCommand("mount --types proc /proc /mnt/gentoo/proc && mount --rbind /sys /mnt/gentoo/sys && mount --make-rslave /mnt/gentoo/sys &&mount --rbind /dev /mnt/gentoo/dev && mount --make-rslave /mnt/gentoo/dev && mount --bind /run /mnt/gentoo/run && mount --make-slave /mnt/gentoo/run");
     executeCommand("mkdir -p /mnt/gentoo/tmp/build && mv ./* /mnt/gentoo/tmp/build/");
-    executeCommand("chroot /mnt/gentoo/ && ./tmp/build/installer_part2");
+    executeCommand("chroot /mnt/gentoo/ && source /etc/profile && export PS1=\"(chroot) ${PS1}\" && /tmp/build/installer_part2");
 }
