da7fa709bd56adccd294848f0ac9a78c258fdef0
lislisowni2 <rafalkozikowski735@gmail.com>
2026-01-28 13:16:23 +0100

    Apply sort for zones

--- scripts/subZone.sh
+++ scripts/subZone.sh
@@ -6,6 +6,8 @@ ZONES=$(find "/usr/share/zoneinfo/$ROOT_ZONE" -type f)
 for ZONE in ${ZONES[@]}; do
     RES=${ZONE##*/};
     if [[ \"$RES\" != \"zoneinfo\" ]]; then
-        echo "$RES" >> /tmp/subZone.tmp
+        echo "$RES" >> /tmp/UnsortedSubZone.tmp
     fi
 done
+
+sort --output=/tmp/subZone.tmp /tmp/UnsortedSubZone.tmp
--- scripts/zone.sh
+++ scripts/zone.sh
@@ -5,6 +5,8 @@ ZONES=$(find /usr/share/zoneinfo -type d)
 for ZONE in ${ZONES[@]}; do
     RES=${ZONE##*/};
     if [[ \"$RES\" != \"zoneinfo\" ]]; then
-        echo "$RES" >> /tmp/zone.tmp
+        echo "$RES" >> /tmp/UnsortedZone.tmp
     fi
 done
+
+sort --output=/tmp/zone.tmp /tmp/UnsortedZone.tmp
