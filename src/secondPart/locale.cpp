1a18128058efee1d757788b72956586e364c87fc
lislisowni2 <rafalkozikowski735@gmail.com>
2026-07-02 14:17:34 +0200

    Add FTXUI to locale.cpp

--- src/secondPart/locale.cpp
+++ src/secondPart/locale.cpp
@@ -7,17 +7,21 @@
 #include <vector>
 #include <fstream>
 
+#include <ftxui/dom/elements.hpp>
+#include <ftxui/component/component.hpp>
+#include <ftxui/component/app.hpp>
+#include <ftxui/component/event.hpp>
+
+using namespace ftxui;
+
 void InstallerSecond::localeSelection() {
     executeCommand("(cat /etc/locale.gen | tail -n+3 | awk '{ print $2 }') > /tmp/localeValues.tmp");
     executeCommand("(cat /etc/locale.gen | tail -n+3 | awk \'{ s = \"\"; for (i = 4; i <= NF; i++) s = s \" \" $i; print s }\') > /tmp/localeNames.tmp");
     std::ifstream localeNameFile("/tmp/localeNames.tmp");
     std::ifstream localeValueFile("/tmp/localeValues.tmp");
     std::string line;
-    int value;
-    std::vector<OptionMenu<std::string>> options = {};
     std::vector<std::string> names = {};
     std::vector<std::string> values = {};
-    int index = 0;
     while (getline(localeNameFile, line)) {
         if (!line.empty() && line[line.length() - 1] == '\n') {
             line.erase(line.length() - 1);
@@ -33,36 +37,92 @@ void InstallerSecond::localeSelection() {
 
         values.push_back(line);
     }
+    
+    int selected = 0;
 
-    for (int i = 0; i < names.size(); i++) {
-        options.push_back(OptionMenu(values[i] + " " + names[i], index, values[i]));
-        index++;
-    }
+    auto screen = App::Fullscreen();
 
-    clearScreen();
-    int key = selectMenu(options, "List of available locales", "Choose correct locale");
+    auto menu = Menu(&names, &selected);
+
+    auto layout = Renderer(menu, [&] {
+        return vbox({
+            text(" GentooInstall ") | bold | center | border,
+            separator(),
+            text("Use UP/DOWN arrow keys to navigate. Press ENTER to select"),
+            separator(),
+            menu->Render() | vscroll_indicator | frame | border | size(HEIGHT, LESS_THAN, 15),
+        });
+    });
+
+    auto inputHandler = CatchEvent(layout, [&](Event event) {
+        if(event == Event::Return) {
+            screen.ExitLoopClosure()();
+            return true;
+        }
+
+        return false;
+    });
+
+    screen.Loop(inputHandler);
+
     std::cout << "\n";
-    if (options[key].value.find('@') == std::string::npos) {
-        executeCommand("echo \"" + options[key].value + ".UTF-8 UTF-8\" >> /etc/locale.gen");
+    if (values[selected].find('@') == std::string::npos) {
+        executeCommand("echo \"" + values[selected] + ".UTF-8 UTF-8\" >> /etc/locale.gen");
     } else {
-        std::string newString = options[key].value.substr(0, options[key].value.find("@") - 1) + ".UTF-8" + options[key].value.substr(options[key].value.find("@") - 1) + " UTF-8";
+        std::string newString = values[selected].substr(0, values[selected].find("@") - 1) + ".UTF-8" + values[selected].substr(values[selected].find("@") - 1) + " UTF-8";
         executeCommand("echo \"" + newString + "\" > /etc/locale.gen");
     }
 }
 
 void InstallerSecond::localeConfig() {
-    std::vector<OptionMenu<std::string>> options = {
-        OptionMenu("Add locale", 1),
-        OptionMenu("Next", -1),
+    std::vector<std::string> locales = {};
+
+    std::vector<std::string> options = {
+        "Add locale",
+        "Continue",
     };
 
+    // while (true) {
+    //     localeSelection();
+    //     clearScreen();
+    //     int key = selectMenu(options, "Do you want add more locales?", "Choose correct option");
+    //     std::cout << "\n";
+    //     if (key == -1) {
+    //         break;
+    //     }
+    // }
+
+    int selected = 0;
+
+    auto screen = App::Fullscreen();
+
+    auto menu = Menu(&options, &selected);
+
+    auto layout = Renderer(menu, [&] {
+        return vbox({
+            text(" GentooInstall ") | bold | center | border,
+            separator(),
+            text("Use UP/DOWN arrow keys to navigate. Press ENTER to select"),
+            separator(),
+            menu->Render() | vscroll_indicator | frame | border | size(HEIGHT, LESS_THAN, 15),
+        });
+    });
+
+    auto inputHandler = CatchEvent(layout, [&](Event event) {
+        if(event == Event::Return) {
+            screen.ExitLoopClosure()();
+            return true;
+        }
+
+        return false;
+    });
+
     while (true) {
-        localeSelection();
-        clearScreen();
-        int key = selectMenu(options, "Do you want add more locales?", "Choose correct option");
-        std::cout << "\n";
-        if (key == -1) {
+        screen.Loop(inputHandler);
+        if (options[selected] == "Continue") {
             break;
+        } else {
+            localeSelection();
         }
     }
 
@@ -78,12 +138,16 @@ void InstallerSecond::localeConfig() {
             line.erase(line.length() - 1);
         }
 
-        options.push_back(OptionMenu<std::string>(line, index));
+        options.push_back(line);
         index++;
     }
 
-    clearScreen();
-    int key = selectMenu(options, "List of generated locales", "Choose correct locale");
-    std::cout << "\n";
-    executeCommand("eselect locale set " + options[key].title);
+    selected = 0;
+
+    screen.Loop(inputHandler);
+
+    // clearScreen();
+    // int key = selectMenu(options, "List of generated locales", "Choose correct locale");
+    // std::cout << "\n";
+    executeCommand("eselect locale set " + options[selected]);
 }
